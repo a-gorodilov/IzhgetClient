5732c4cf9570586b3741c5a1b98580217c8ae1eb
agorodilov <mail@grdlv.ru>
2018-11-07 20:55:00 +0400

    Created new file with base Izhget request const params

---
+++ IzhgetClient/Client/IzhgetRequestParams.cs
@@ -0,0 +1,8 @@
+namespace Client
+{
+    public static class IzhgetRequestParams
+    {
+        public const string Url = "http://xn--c1aff6b0c.xn--p1ai/rasp/load_station.php";
+        public const string ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+    }
+}
